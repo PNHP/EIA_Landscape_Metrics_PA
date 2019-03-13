3e5068e1fe2519a4a1368815186a3f68c0513d80
Christopher Tracey <ctracey@paconserve.org>
2019-03-12 21:06:10 -0400

    working LAN1

--- Level1_EIA.py
+++ Level1_EIA.py
@@ -41,14 +41,17 @@ outExtractByMask = ExtractByMask(landcover, site_buffer)
 
 # load the various lookup tables
 lu_reclass = "lu_NLCD2011_remapNatCov"
+
 # Execute Reclassify
 arcpy.AddMessage("Reclassify the landcover to natural cover.")
 NatCov = ReclassByTable(outExtractByMask, lu_reclass, "Value", "Value", "CoverTypeval", "NODATA")
 NatCov.save("Site_NatCover")
 
+################################################################################
+## LAN1  Contiguous Natural Buffer #############################################
+
 # region group
 arcpy.AddMessage("Create Region Groups.")
-outRgnGrp = "tmp_RegionGroups"
 outRgnGrp = RegionGroup(NatCov, "FOUR")
 # extract to points and extract from region group
 tmp_points = "outSitePoints"
@@ -57,21 +60,92 @@ arcpy.FeatureToPoint_management(site, tmp_points, "INSIDE")
 ExtractValuesToPoints(tmp_points, outRgnGrp, tmp_points1, "NONE", "ALL")
 arcpy.AddMessage("Trying to get regiongroup id")
 
-cursor = arcpy.da.SearchCursor(tmp_points1, ['RASTERVALU'])
-for row in cursor:
-    print(row[0])
-    inSQLClause = '"' + "Value" + '"  = "' + str(row[0]) + '"'
-    attRgnGrp = ExtractByAttributes(inRaster, inSQLClause)
-    attExtract.save("tmp_ContNatCover")
+with arcpy.da.SearchCursor(tmp_points1, ['RASTERVALU']) as cursor:
+    for row in cursor:
+        extractvalue = str(row[0])
 
-#with arcpy.da.SearchCursor(tmp_points1, ['RASTERVALU']) as cursor:
-#    for row in cursor:
-#        x = {0}
-#        #print('{0}'.format(row[0]))
+inSQLClause = "Value ={0}".format(extractvalue)
+attRgnGrp = ExtractByAttributes(outRgnGrp, inSQLClause)
+attRgnGrp.save("tmp_ContNatCover")
 
-#attRgnGrp = ExtractByAttributes(outRgnGrp, '"' + "Value" + '"  = "' + str(x) + '"' )
+# calculate the score
+attRgnGrp = arcpy.BuildRasterAttributeTable_management(attRgnGrp, "Overwrite")
+with arcpy.da.SearchCursor(attRgnGrp, ['Count']) as cursor:
+    for row in cursor:
+        NatCovCellCount = row[0]
+
+outExtractByMask1 = arcpy.BuildRasterAttributeTable_management(outExtractByMask, "Overwrite")
+AllCellCount = 0
+with arcpy.da.SearchCursor(outExtractByMask1, ['Count']) as cursor:
+    for row in cursor:
+        AllCellCount += row[0]
+
+NatCovProp = NatCovCellCount / AllCellCount
+NatCovProp = round(NatCovProp, 3)
+
+# calculate the rating
+if NatCovProp >= 0.9:
+    NatCovScore = "A"
+elif NatCovProp >= 0.6 and NatCovProp < 0.9:
+    NatCovScore = "B"
+elif NatCovProp >= 0.2 and NatCovProp < 0.6:
+    NatCovScore = "C"
+else:
+    NatCovScore = "D"
+
+# report it out
+arcpy.AddMessage("----------------------------------------")
+arcpy.AddMessage("LAN1 - Contiguous Natural Buffer -------")
+arcpy.AddMessage("Score: {0}".format(str(NatCovProp)))
+arcpy.AddMessage("Rating: {0}".format(str(NatCovScore)))
+arcpy.AddMessage("----------------------------------------")
+
+
+################################################################################
+## LAN2 -  #############################################
 
 
 
 
+# report it out
+arcpy.AddMessage("----------------------------------------")
+arcpy.AddMessage("LAN2 - A -------")
+#arcpy.AddMessage("Score: {0}".format(str(NatCovProp)))
+#arcpy.AddMessage("Rating: {0}".format(str(NatCovScore)))
+arcpy.AddMessage("----------------------------------------")
+
+
+
+
+################################################################################
+## BUF1 -  #############################################
+
+
+
+
+# report it out
+arcpy.AddMessage("----------------------------------------")
+arcpy.AddMessage("BUF1 - A -------")
+#arcpy.AddMessage("Score: {0}".format(str(NatCovProp)))
+#arcpy.AddMessage("Rating: {0}".format(str(NatCovScore)))
+arcpy.AddMessage("----------------------------------------")
+
+
+
+
+
+
+
+################################################################################
+## BUF2 -  #############################################
+
+
+
+
+# report it out
+arcpy.AddMessage("----------------------------------------")
+arcpy.AddMessage("BUF2 - A -------")
+#arcpy.AddMessage("Score: {0}".format(str(NatCovProp)))
+#arcpy.AddMessage("Rating: {0}".format(str(NatCovScore)))
+arcpy.AddMessage("----------------------------------------")
 
